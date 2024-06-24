8ac7e5fcd4db571dea9e6d021fadeba30c21692c
Priyam Aneja <priyamaneja2002@gmail.com>
2024-06-24 20:19:16 +0530

    styling fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        itc: ['ITC Garamond Book Condensed', 'sans-serif'], // Add your custom font here
+        gtalpina: ['GT Alpina Typewriter', 'sans-serif'],
+        apercu: ['Apercu', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     function({ addVariant, e }) {
